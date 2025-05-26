881b0953e392d0fb91f8af77c8cb414dc696344d
linharesrocha <guilhermelinharesr@gmail.com>
2025-05-26 14:59:07 -0300

    feat(service): initialize EasyLink core service structure

---
+++ easylink-service/src/main/java/br/com/easylink/easylink_service/EasylinkServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.easylink.easylink_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylinkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasylinkServiceApplication.class, args);
+	}
+
+}
---
+++ easylink-service/src/test/java/br/com/easylink/easylink_service/EasylinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easylink.easylink_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
