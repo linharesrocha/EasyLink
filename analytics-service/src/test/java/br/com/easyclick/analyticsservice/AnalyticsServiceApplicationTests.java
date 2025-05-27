264005da870ee8c81a50b5ebbd8678967999c570
linharesrocha <guilhermelinharesr@gmail.com>
2025-05-26 23:43:04 -0300

    feat(analytics): initialize service and infra for click tracking

---
+++ analytics-service/src/test/java/br/com/easyclick/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easyclick.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
