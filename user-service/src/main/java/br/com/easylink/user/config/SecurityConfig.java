8a6379df1b887d71486d531cd65f76760020b627
linharesrocha <guilhermelinharesr@gmail.com>
2025-05-27 01:19:02 -0300

    feat(user-service): define user entity and initial security config

---
+++ user-service/src/main/java/br/com/easylink/user/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package br.com.easylink.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 3. Definindo as regras de autorização para os endpoints.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 4. Definindo o BCrypt como o algoritmo para hashing de senhas.
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ user-service/src/test/java/br/com/easylink/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easylink.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
