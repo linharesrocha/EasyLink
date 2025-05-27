0266a57615eb0716a6d9cd6d596285cae9404f33
linharesrocha <guilhermelinharesr@gmail.com>
2025-05-27 00:22:59 -0300

    feat(easylink-service): implement redirect caching with redis

--- easylink-service/src/main/java/br/com/easylink/easylinkservice/EasylinkServiceApplication.java
+++ easylink-service/src/main/java/br/com/easylink/easylinkservice/EasylinkServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.easylink.easylinkservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EasylinkServiceApplication {
 
 	public static void main(String[] args) {
--- easylink-service/src/main/java/br/com/easylink/easylinkservice/application/RedirectUseCaseImpl.java
+++ easylink-service/src/main/java/br/com/easylink/easylinkservice/application/RedirectUseCaseImpl.java
@@ -3,6 +3,7 @@ package br.com.easylink.easylinkservice.application;
 import br.com.easylink.easylinkservice.application.ports.RedirectUseCase;
 import br.com.easylink.easylinkservice.application.ports.UrlMappingRepositoryPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class RedirectUseCaseImpl implements RedirectUseCase {
     private static final String URL_CLICKS_TOPIC = "url-clicks-topic";
 
     @Override
+    @Cacheable(value = "redirects", key = "#shortKey")
     public Optional<String> getOriginalUrl(String shortKey) {
         return urlMappingRepositoryPort.findByShortKey(shortKey)
                 .map(urlMapping -> {
