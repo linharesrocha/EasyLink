09e0be0731034431491a3fd96c9b761972f464ef
linharesrocha <guilhermelinharesr@gmail.com>
2025-05-26 23:56:47 -0300

    feat(easylink-service): publish click events for kafka on redirect

--- easylink-service/src/main/java/br/com/easylink/easylinkservice/application/RedirectUseCaseImpl.java
+++ easylink-service/src/main/java/br/com/easylink/easylinkservice/application/RedirectUseCaseImpl.java
@@ -3,6 +3,7 @@ package br.com.easylink.easylinkservice.application;
 import br.com.easylink.easylinkservice.application.ports.RedirectUseCase;
 import br.com.easylink.easylinkservice.application.ports.UrlMappingRepositoryPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,10 +13,18 @@ import java.util.Optional;
 public class RedirectUseCaseImpl implements RedirectUseCase {
 
     private final UrlMappingRepositoryPort urlMappingRepositoryPort;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String URL_CLICKS_TOPIC = "url-clicks-topic";
 
     @Override
     public Optional<String> getOriginalUrl(String shortKey) {
         return urlMappingRepositoryPort.findByShortKey(shortKey)
-                .map(urlMapping -> urlMapping.getOriginalUrl());
+                .map(urlMapping -> {
+
+                    kafkaTemplate.send(URL_CLICKS_TOPIC, urlMapping.getShortKey());
+
+                    return urlMapping.getOriginalUrl();
+                });
     }
 }
